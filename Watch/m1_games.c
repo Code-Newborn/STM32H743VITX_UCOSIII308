ee2fb56e9f63afe4ac2f9e47e53f03e46ec543e1
Code-Newborn <1253709179@qq.com>
2023-12-10 23:32:44 +0800

    [update] 更改游戏图标

--- Watch/m1_games.c
+++ Watch/m1_games.c
@@ -15,48 +15,44 @@ static prev_menu_s prevMenuData;
 static void mSelect(void);
 static void itemLoader(byte);
 
-static uint8_t getItemCount()
-{
-	uint8_t cnt = 0;
+static uint8_t getItemCount() {
+    uint8_t cnt = 0;
 #if COMPILE_GAME1
-	++cnt;
+    ++cnt;
 #endif
 #if COMPILE_GAME2
-	++cnt;
+    ++cnt;
 #endif
 #if COMPILE_GAME3
-	++cnt;
+    ++cnt;
 #endif
-	return cnt;
+    return cnt;
 }
 
-void mGamesOpen()
-{
-	setMenuInfo(OPTION_COUNT, MENU_TYPE_STR, PSTR(STR_GAMESMENU));
-	setMenuFuncs(MENUFUNC_NEXT, mSelect, MENUFUNC_PREV, itemLoader);
+void mGamesOpen() {
+    setMenuInfo(OPTION_COUNT, MENU_TYPE_ICON, PSTR(STR_GAMESMENU));
+    setMenuFuncs(MENUFUNC_NEXT, mSelect, MENUFUNC_PREV, itemLoader);
 
-	setPrevMenuOpen(&prevMenuData, mGamesOpen);
+    setPrevMenuOpen(&prevMenuData, mGamesOpen);
 
-	beginAnimation2(NULL);
+    beginAnimation2(NULL);
 }
 
-static void mSelect()
-{
-	setPrevMenuExit(&prevMenuData);
-	doAction(true);
+static void mSelect() {
+    setPrevMenuExit(&prevMenuData);
+    doAction(true);
 }
 
-static void itemLoader(byte num)
-{
-	num = 0;
+static void itemLoader(byte num) {
+    num = 0;
 #if COMPILE_GAME1
-	setMenuOption_P(num++, PSTR(GAME1_NAME), NULL, game1_start);
+    setMenuOption_P(num++, PSTR(GAME1_NAME), menu_game1, game1_start);
 #endif
 #if COMPILE_GAME2
-	setMenuOption_P(num++, PSTR(GAME2_NAME), NULL, game2_start);
+    setMenuOption_P(num++, PSTR(GAME2_NAME), menu_game2, game2_start);
 #endif
 #if COMPILE_GAME3
-	setMenuOption_P(num++, PSTR(GAME3_NAME), NULL, game3_start);
+    setMenuOption_P(num++, PSTR(GAME3_NAME), menu_game3, game3_start);
 #endif
-	addBackOption();
+    addBackOption();
 }
