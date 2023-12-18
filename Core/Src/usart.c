947ec28c12115bdc254d6fb38d61d7ff2a94ced9
Code-Newborn <1253709179@qq.com>
2023-12-18 21:44:06 +0800

    [update] 串口输入输出重定向

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -33,36 +33,36 @@ __asm( ".global __ARM_use_no_argv \n\t" );   /* AC6下需要声明main函数为�
 
 #else
 /* 使用AC5编译器时, 要在这里定义__FILE 和 不使用半主机模式 */
-// #pragma import( __use_no_semihosting )
+#pragma import( __use_no_semihosting )
 
-// struct __FILE {
-//     int handle;
-//     /* Whatever you require here. If the only file you are using is */
-//     /* standard output using printf() for debugging, no file handling */
-//     /* is required. */
-// };
+struct __FILE {
+    int handle;
+    /* Whatever you require here. If the only file you are using is */
+    /* standard output using printf() for debugging, no file handling */
+    /* is required. */
+};
 
 #endif
 
-// /* 不使用半主机模式，至少需要重定义_ttywrch\_sys_exit\_sys_command_string函数,以同时兼容AC6和AC5模式 */
-// int _ttywrch( int ch ) {
-//     ch = ch;
-//     return ch;
-// }
+/* 不使用半主机模式，至少需要重定义_ttywrch\_sys_exit\_sys_command_string函数,以同时兼容AC6和AC5模式 */
+int _ttywrch( int ch ) {
+    ch = ch;
+    return ch;
+}
 
-// /* 定义_sys_exit()以避免使用半主机模式 */
-// void _sys_exit( int x ) {
-//     x = x;
-// }
+/* 定义_sys_exit()以避免使用半主机模式 */
+void _sys_exit( int x ) {
+    x = x;
+}
 
-// char* _sys_command_string( char* cmd, int len ) {
-//     return NULL;
-// }
+char* _sys_command_string( char* cmd, int len ) {
+    return NULL;
+}
 
 /* FILE 在 stdio.h里面定义. */
-// FILE __stdout;
+FILE __stdout, __stdin;
 
-// /* 重定义fputc函数, printf函数最终会通过调用fputc输出字符串到串口 */
+/* 重定义fputc函数, printf函数最终会通过调用fputc输出字符串到串口 */
 // int fputc( int ch, FILE* f ) {
 //     while ( ( USART1->ISR & 0X40 ) == 0 )  // INFO 使用USART1
 //         ;                                  /* 等待上一个字符发送完成 */
@@ -71,14 +71,14 @@ __asm( ".global __ARM_use_no_argv \n\t" );   /* AC6下需要声明main函数为�
 //     return ch;
 // }
 
-/// 重定向c库函数printf到串口USARTx，重定向后可使用printf函数
+/* 重定向c库函数printf到串口USARTx，重定向后可使用printf函数 */
 int fputc( int ch, FILE* f ) {
     /* 发送一个字节数据到串口USARTx */
     HAL_UART_Transmit( &huart1, ( uint8_t* )&ch, 1, 0xFFFF );  // INFO 使用USART1
     return ( ch );
 }
 
-/// 重定向c库函数scanf到串口USARTx，重写向后可使用scanf、getchar等函数
+/* 重定向c库函数scanf到串口USARTx，重写向后可使用scanf、getchar等函数 */
 int fgetc( FILE* f ) {
     int ch;
     /* 等待串口输入数据 */
