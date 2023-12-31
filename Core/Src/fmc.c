d0c6ad3356a7ffbb273094fd2fba1d5ba695b731
Code-Newborn <1253709179@qq.com>
2023-12-31 16:07:26 +0800

    [update] 成功获取LCD_ID，与优化等级-O3无关

--- Core/Src/fmc.c
+++ Core/Src/fmc.c
@@ -298,11 +298,11 @@ static void HAL_FMC_MspInit( void ) {
     PD13   ------> FMC_A18
     PD14   ------> FMC_D0
     PD15   ------> FMC_D1
-    PC7   ------> FMC_NE1
     PD0   ------> FMC_D2
     PD1   ------> FMC_D3
     PD4   ------> FMC_NOE
     PD5   ------> FMC_NWE
+    PD7   ------> FMC_NE1
     */
     /* GPIO_InitStruct */
     GPIO_InitStruct.Pin       = GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
@@ -314,22 +314,13 @@ static void HAL_FMC_MspInit( void ) {
     HAL_GPIO_Init( GPIOE, &GPIO_InitStruct );
 
     /* GPIO_InitStruct */
-    GPIO_InitStruct.Pin       = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5;
+    GPIO_InitStruct.Pin       = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_7;
     GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull      = GPIO_NOPULL;
     GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
 
     HAL_GPIO_Init( GPIOD, &GPIO_InitStruct );
-
-    /* GPIO_InitStruct */
-    GPIO_InitStruct.Pin       = GPIO_PIN_7;
-    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull      = GPIO_NOPULL;
-    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF9_FMC;
-
-    HAL_GPIO_Init( GPIOC, &GPIO_InitStruct );
 
     /* USER CODE BEGIN FMC_MspInit 1 */
 
@@ -375,18 +366,16 @@ static void HAL_FMC_MspDeInit( void ) {
     PD13   ------> FMC_A18
     PD14   ------> FMC_D0
     PD15   ------> FMC_D1
-    PC7   ------> FMC_NE1
     PD0   ------> FMC_D2
     PD1   ------> FMC_D3
     PD4   ------> FMC_NOE
     PD5   ------> FMC_NWE
+    PD7   ------> FMC_NE1
     */
 
     HAL_GPIO_DeInit( GPIOE, GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 );
 
-    HAL_GPIO_DeInit( GPIOD, GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5 );
-
-    HAL_GPIO_DeInit( GPIOC, GPIO_PIN_7 );
+    HAL_GPIO_DeInit( GPIOD, GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_7 );
 
     /* USER CODE BEGIN FMC_MspDeInit 1 */
 
