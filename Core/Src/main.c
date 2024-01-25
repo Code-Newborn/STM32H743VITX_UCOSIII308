fafdf415bf0650797d2743ad9d3234045f8392ec
Code-Newborn <12537091X@qq.com>
2024-01-25 14:53:32 +0800

    [update] 调整字符标签显示位置

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -105,18 +105,19 @@ void Lvgl_Lable_Demo( void ) {
     lv_obj_t* scr    = lv_scr_act();
     lv_obj_t* label1 = lv_label_create( scr );
     lv_label_set_long_mode( label1, LV_LABEL_LONG_WRAP );
+
     lv_label_set_recolor( label1, true );
+    lv_obj_set_width( label1, 150 );
     lv_label_set_text( label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label, align the lines to the center "
                                "and wrap long text automatically." );
-    lv_obj_set_width( label1, 150 );
     lv_obj_set_style_text_align( label1, LV_TEXT_ALIGN_CENTER, 0 );
-    lv_obj_align( label1, LV_ALIGN_CENTER, 0, -40 );
+    lv_obj_align( label1, LV_ALIGN_CENTER, 0, -80 );
 
     lv_obj_t* label2 = lv_label_create( scr );
     lv_label_set_long_mode( label2, LV_LABEL_LONG_SCROLL_CIRCULAR );
     lv_obj_set_width( label2, 150 );
     lv_label_set_text( label2, "It is a circularly scrolling text. " );
-    lv_obj_align( label2, LV_ALIGN_CENTER, 0, 40 );
+    lv_obj_align( label2, LV_ALIGN_CENTER, 0, 60 );
 }
 
 /* USER CODE END 0 */
@@ -161,7 +162,7 @@ int main( void ) {
     lv_init();
     lv_port_disp_init();
 
-    lv_ex_label();
+    // lv_ex_label();
     Lvgl_Lable_Demo();
     lv_example_led_1();
 
