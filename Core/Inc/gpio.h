d596c427a4c7e79e13abead61644bd02fdce31c4
Code-Newborn <12537091X@qq.com>
2024-02-21 19:01:33 +0800

    [update] CubeMX的STM32H743固件FW升级

--- Core/Inc/gpio.h
+++ Core/Inc/gpio.h
@@ -1,21 +1,22 @@
+/* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    gpio.h
-  * @brief   This file contains all the function prototypes for
-  *          the gpio.c file
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    gpio.h
+ * @brief   This file contains all the function prototypes for
+ *          the gpio.c file
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __GPIO_H__
 #define __GPIO_H__
@@ -35,7 +36,7 @@ extern "C" {
 
 /* USER CODE END Private defines */
 
-void MX_GPIO_Init(void);
+void MX_GPIO_Init( void );
 
 /* USER CODE BEGIN Prototypes */
 
@@ -45,5 +46,3 @@ void MX_GPIO_Init(void);
 }
 #endif
 #endif /*__ GPIO_H__ */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -64,6 +64,7 @@ void Error_Handler( void );
 #define WiFi_TX_GPIO_Port GPIOC
 #define WiFi_RX_Pin       GPIO_PIN_11
 #define WiFi_RX_GPIO_Port GPIOC
+
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
@@ -73,5 +74,3 @@ void Error_Handler( void );
 #endif
 
 #endif /* __MAIN_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
