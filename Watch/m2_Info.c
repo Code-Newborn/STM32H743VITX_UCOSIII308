51feff91b6f1f55673afe7fb4061ed0519f4e010
Code-Newborn <12537091X@qq.com>
2024-05-21 00:34:24 +0800

    [update] 子菜单返回上级，按键功能绑定失效

--- Watch/m2_Info.c
+++ Watch/m2_Info.c
@@ -12,16 +12,24 @@ static bool      down( void );
 static bool      up( void );
 static bool      select( void );
 static display_t draw( void );
+// static void      itemLoader( byte );
 
 
 void mInfoOpen() {
-    display_setDrawFunc( draw );
-    buttons_setFuncs( up, select, down );
-    setMenuInfo( OPTION_COUNT, MENU_TYPE_STR, PSTR( STR_INFOSMENU ) );
+    // menuData.func.draw = draw;
+    display_setDrawFunc( draw );           // 设置绘图功能函数
+    buttons_setFuncs( down, select, up );  // 绑定按键功能，重置、启停、退出
+
     setPrevMenuOpen( &prevMenuData, mInfoOpen );
 
-    animation_start( NULL, ANIM_MOVE_ON );
+    animation_start( NULL, ANIM_MOVE_ON );  // 打开动画动画过度
 }
+
+// static void itemLoader( byte num ) {
+//     UNUSED( num );
+//     showAlarmStr( num, &alarm2 );  // 遍历打印
+//     addBackOption();
+// }
 
 static bool down() {
     idx--;
@@ -36,12 +44,14 @@ static bool up() {
 }
 
 static display_t draw() {
-    draw_bitmap( 25, 20, dot_11x16, FONT_DOT_WIDTH, FONT_DOT_HEIGHT, NOINVERT, 0 );
+    draw_bitmap( 48, 20, close32x16, FONT_CLOSE_WIDTH, FONT_CLOSE_HEIGHT, NOINVERT, 0 );
 
-    return DISPLAY_BUSY;
+    return DISPLAY_DONE;
 }
 
 static bool select() {
-    setPrevMenuExit( &prevMenuData );
-    doAction( true );
+    animation_start( back, ANIM_MOVE_OFF );
+    prevMenuData.lastSelected = 0;                  // Reset selected item
+    menuData.prevMenu         = prevMenuData.last;  //
+    return true;
 }
